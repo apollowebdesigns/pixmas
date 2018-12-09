415d81f73bd9ea2cb65930f037d0693bad0b955a
Andy Evans <andrewevans0@outlook.com>
2018-12-09 17:30:12 +0000

    wip

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 // const boards = require('js-gpiozero/gpiozero/boards');
-const LEDBoard = require('js-gpiozero/gpiozero/boards/LEDBoard');
+const LEDBoard = require('gpiozero').LEDBoard;
 console.log(JSON.stringify(LEDBoard));
-const {random_values} = require('gpiozero/tools');
-let numbers = [];
-for(let i = 2; i < 28; i++) {
-    numbers.push(i);    
-}
-const tree = LEDBoard(numbers, True);
-for (led of tree){
-    led.source_delay = 0.1
-    led.source = random_values()
-    if(this.on == False) {
-        break
-    }
-}
+const leds = new LEDBoard([2, 3, 4, 5, 6]);
+leds.on();
             
