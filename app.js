dcf0f0cb3f4d747e711b788e83e4d36c6e25131c
Andy Evans <andrewevans0@outlook.com>
2018-12-09 17:13:32 +0000

    wip

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // const boards = require('js-gpiozero/gpiozero/boards');
 const {LEDBoard} = require('js-gpiozero/gpiozero/boards/LEDBoard');
-console.log(JSON.stringify(boards));
 console.log(JSON.stringify(LEDBoard));
 const {random_values} = require('gpiozero/tools');
 let numbers = [];
