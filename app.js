bdb946ad9598e8375db72a8fb13dbbfa93a7b1e3
Andy Evans <andrewevans0@outlook.com>
2018-12-09 16:53:48 +0000

    added in tree changes

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ for(let i = 2; i < 28; i++) {
     numbers.push(i);    
 }
 const tree = LEDBoard(numbers, True);
+for (led of tree){
+    led.source_delay = 0.1
+    led.source = random_values()
+    if(this.on == False) {
+        break
+    }
+}
+            
